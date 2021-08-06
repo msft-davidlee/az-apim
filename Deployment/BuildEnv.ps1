d4bbd86a4f2373d985b8152ed32812a21cd42ab6
David Lee <leedavid@microsoft.com>
2021-08-06 11:49:47 -0500

    deploy api

--- Deployment/BuildEnv.ps1
+++ Deployment/BuildEnv.ps1
@@ -21,27 +21,4 @@ $deployText = (az deployment group create --name $deploymentName --resource-grou
 $deployOutput = ($deployText | ConvertFrom-Json)
 $serviceName = $deployOutput.properties.outputs.apimName.value
 
-$ApiMgmtContext = New-AzApiManagementContext -ResourceGroupName $rgName -ServiceName $serviceName
-$apiList = Get-AzApiManagementApi -Context $ApiMgmtContext
-
-$customerService = "Customer Service Rewards API"
-$customerServiceApiFound = $apiList | Where-Object { $_.Name -eq $customerService }
-
-if (!$customerServiceApiFound) {
-    New-AzApiManagementApi -Context $ApiMgmtContext -Name $customerService -Protocols @("https") -Path "rewards"
-    
-    $MemberId = New-Object -TypeName Microsoft.Azure.Commands.ApiManagement.ServiceManagement.Models.PsApiManagementParameter
-    $MemberId.Name = "MemberId"
-    $MemberId.Description = "Member Id"
-    $MemberId.Type = "string"
-
-    $ResponseRepresentation = New-Object -TypeName Microsoft.Azure.Commands.ApiManagement.ServiceManagement.Models.PsApiManagementRepresentation
-    $ResponseRepresentation.ContentType = 'application/json'
-    $ResponseRepresentation.Sample = '{ "points": [ { "value":100, "effective":"2021-08-01",  "expires":"2021-08-31" } ] }'
-
-    $Response = New-Object -TypeName Microsoft.Azure.Commands.ApiManagement.ServiceManagement.Models.PsApiManagementResponse
-    $Response.StatusCode = 204
-    $Response.Representations = @($ResponseRepresentation)
-    
-    New-AzApiManagementOperation -Context $ApiMgmtContext -ApiId "LookupRewardPoints" -OperationId "61234567890" -Name 'Lookup reward points' -Method 'GET' -UrlTemplate '/rewards/{memberId}/points' -Description "Use this operation to lookup rewards points." -TemplateParameters @($MemberId) -Responses @($Response)
-}
+Write-Host "::set-output name=serviceName::$serviceName"
